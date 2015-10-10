8d425c8757608c79806bcd64aa0d1f02228b20ee
Russell Hay <me@russellhay.com>
2015-10-09 20:08:48 -0700

    Fixing s3 deployment

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,33 +1,16 @@
 var gulp = require('gulp'),
-    aws = require('gulp-awspublish'),
-    merge = require('merge-stream'),
+    s3 = require('gulp-s3'),
+    gzip = require('gulp-gzip'),
     fs = require('fs');
 
 var home = process.env.HOME || process.env.USERPROFILE;
 var credentials = JSON.parse(fs.readFileSync(home + '/.aws/impulse.json', 'utf8'));
-var publish = aws.create(credentials);
 
-gulp.task('deploy-episodes', function () {
-    var headers = {
-        'Cache-Control': 'max-age=315360000, no-transform, public'
-    };
+gulp.task('deploy', ['default'], function() {
+    gulp.src(['output/**', '!output/audio/**'])
+        .pipe(gzip())
+        .pipe(s3(credentials, { gzippedOnly: true }));
 
-    var mp3s = gulp.src('output/audio/*.mp3');
-    var pages = gulp.src('output/[0-9]*/index.html')
-        .pipe(aws.gzip({ext: '.gz'}));
-
-    merge([mp3s, pages])
-        .pipe(publish.publish(headers))
-        .pipe(publish.cache())
-        .pipe(aws.reporter());
+    gulp.src(['output/audio/**'])
+        .pipe(s3(credentials));
 });
-
-gulp.task('deploy-indices', function() {
-    gulp.src(['output/*.html', '!output/[0-9]*/index.html'])
-        .pipe(aws.gzip({ext: '.gz'}))
-        .pipe(publish.publish())
-        .pipe(publish.cache())
-        .pipe(aws.reporter());
-});
-
-gulp.task('deploy', ['default', 'deploy-indices', 'deploy-episodes']);
