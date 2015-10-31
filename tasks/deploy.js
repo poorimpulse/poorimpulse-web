1b890cad0231bb204b394bd48bc9af94379da232
Russell Hay <me@russellhay.com>
2015-10-31 12:23:09 -0700

    split up deploy so that I can be more targetted when updating"

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -6,11 +6,29 @@ var gulp = require('gulp'),
 var home = process.env.HOME || process.env.USERPROFILE;
 var credentials = JSON.parse(fs.readFileSync(home + '/.aws/impulse.json', 'utf8'));
 
-gulp.task('deploy', ['default'], function() {
-    gulp.src(['output/**', '!output/audio/**', '!output/feed.rss'])
+gulp.task('deploy', ['deploy-gzip', 'deploy-audio', 'deploy-feed', 'deploy-images']);
+
+gulp.task('deploy-gzip', ['default'], function() {
+    gulp.src(['output/**', '!output/*.png', '!output/audio/**', '!output/feed.rss'])
         .pipe(gzip())
         .pipe(s3(credentials, { gzippedOnly: true }));
+});
 
-    gulp.src(['output/**/*.mp3', 'output/feed.rss'])
+gulp.task('deploy-images', ['static-files'], function() {
+    gulp.src('output/*.png')
+        .pipe(s3(credentials), {
+           'Cache-Control': 'max-age=604800,no-transform,public'
+        });
+});
+
+gulp.task('deploy-audio', ['default'], function() {
+    gulp.src(['output/**/*.mp3'])
         .pipe(s3(credentials));
+});
+
+gulp.task('deploy-feed', ['rss'], function() {
+    gulp.src(['output/feed.rss'])
+        .pipe(s3(credentials, {
+            'Cache-Control': 'max-age=604800, no-transform, public'
+        }));
 });
